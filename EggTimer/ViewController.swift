40a515f924e6a2f4c4b045dff16ea859ec9fedac
hvaandres <hvaandres@icloud.com>
2020-06-17 21:58:03 -0600

    Comments added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -26,16 +26,27 @@ class ViewController: UIViewController {
         
         timer.invalidate()
         
+        // Create a variable that will render the title of eggTimes
+        
         let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        // Call the variable totalTimes and make it equal to the array to know the seconds and raped it 
         
         totalTime = eggTimes[hardness]!
         
+        // We need to make sure that the app will be able to reset the progress bar
+        
         progreeBar.progress = 0.0
         secondsPassed = 0
+        // We need to make sure that the title will change
         titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:  #selector(updateTimer), userInfo:nil, repeats: true)
     }
+    
+    // Create a function that will do the calculation for the seconds and the progress bar.
+    
+    // It is important to show the progress bar to make sure the end-user can see the progress through the time
     
     @objc func updateTimer() {
         if secondsPassed < totalTime {
@@ -47,7 +58,9 @@ class ViewController: UIViewController {
 
         } else {
             timer.invalidate()
-            titleLabel.text = "Done!"
+            titleLabel.text = "Done!" // Send a message when is done
+            
+            // Add the sound to make sure will alarm the end-user when the timer will be done.
             
             let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
             player = try! AVAudioPlayer(contentsOf: url!)
